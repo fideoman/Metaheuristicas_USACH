35b0e4ba70a7b8bc32b95572384d307e2594ff77
User <User@10.0.1.24>
2018-06-24 17:56:29 -0300

    Isaac: La busqueda tabú ahora carga archivos QAPLIB sin modificaciones.

--- BusquedaTabu/src/main/java/cl/usach/mh/busquedatabu/BusquedaTabuEjecutable.java
+++ BusquedaTabu/src/main/java/cl/usach/mh/busquedatabu/BusquedaTabuEjecutable.java
@@ -1,21 +1,18 @@
 package cl.usach.mh.busquedatabu;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import org.apache.commons.collections4.bidimap.DualHashBidiMap;
-import org.apache.commons.numbers.combinatorics.Combinations;
+import org.apache.commons.lang3.ArrayUtils;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
@@ -26,10 +23,8 @@ import org.jfree.data.xy.XYSeriesCollection;
 import cl.usach.mh.busquedatabu.qap.Local;
 import cl.usach.mh.busquedatabu.qap.Localidad;
 
-public class BusquedaTabuNUG12 {
-	
-	public static final List<Integer> secuencia = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12); // Secuencia básica fija (12)
-	
+public class BusquedaTabuEjecutable {
+		
 	public static ArrayList<Local> locales = new ArrayList<Local>();
 	public static ArrayList<Localidad> localidades = new ArrayList<Localidad>();	
 	
@@ -43,54 +38,72 @@ public class BusquedaTabuNUG12 {
 		int numeroCiclos = 500; // Repeticiones - Ciclos de búsqueda
 		// Fin de Parámetros	
 		
-		cargarDatosQAPNUG12();
+		cargarDatosQAP("nug12.qap");
 		
 		// Busqueda Tabu
-		BusquedaTabu.ejecucion(BusquedaTabuNUG12.locales, BusquedaTabuNUG12.localidades, tenor, solucionInicial, numeroCiclos);
+		BusquedaTabu.ejecucion(locales, localidades, tenor, solucionInicial, numeroCiclos);
 		
 		grafico("Tenor: " + tenor + " | Mejor Resultado: " + (BusquedaTabu.calculoCosto(BusquedaTabu.mejorSolucionHistorica) == 578 ? "578 (El mejor del mundo)" : BusquedaTabu.calculoCosto(BusquedaTabu.mejorSolucionHistorica) + " (578 es el mejor del mundo)"), "Todas", "Mejor", BusquedaTabu.solucionesEncontradas, BusquedaTabu.mejoresSolucionesHistoricas);
 	}
 	
-	public static void cargarDatosQAPNUG12() throws IOException, URISyntaxException {
-		ClassLoader cargadorClase = Thread.currentThread().getContextClassLoader();
+	public static void cargarDatosQAP(String archivoQAPLIB) throws URISyntaxException {
+		ClassLoader cargadorContexto = Thread.currentThread().getContextClassLoader();
+
+		ArrayList<ArrayList<Integer>> distancias = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> flujos = new ArrayList<ArrayList<Integer>>();
+		int cantidad = 0;
 		
-		int[][] localidadesEnCrudo = new int[12][11];
-		for (int linea = 0; linea < Files.readAllLines(Paths.get(cargadorClase.getResource("nug12d.qap").toURI()), Charset.defaultCharset()).size(); linea++) {
-			localidadesEnCrudo[linea] = Arrays.asList(Files.readAllLines(Paths.get(cargadorClase.getResource("nug12d.qap").toURI()), Charset.defaultCharset()).get(linea).split(" ")).stream().mapToInt(Integer::valueOf).toArray();
-		}   
-		
-		int[][] localesEnCrudo = new int[12][11];
-		for (int linea = 0; linea < Files.readAllLines(Paths.get(cargadorClase.getResource("nug12f.qap").toURI()), Charset.defaultCharset()).size(); linea++) {
-			localesEnCrudo[linea] = Arrays.asList(Files.readAllLines(Paths.get(cargadorClase.getResource("nug12f.qap").toURI()), Charset.defaultCharset()).get(linea).split(" ")).stream().mapToInt(Integer::valueOf).toArray();
-		}    			        
-        
-		for (int i = 1; i <= localidadesEnCrudo.length; i++) {
-			Localidad localidad = new Localidad();
-			int[] conjuntoComplemento = new int[secuencia.size() - 1];
-			for (int j = 1; j <= conjuntoComplemento.length + 1; j++) {
-				if (i == secuencia.get(j - 1)) {
-					ArrayList<Integer> copiaSecuencia = new ArrayList<Integer>(secuencia);
-					for (Iterator<Integer> elementoCopia = copiaSecuencia.iterator(); elementoCopia.hasNext();) {
-						Integer etiqueta = elementoCopia.next();
-						if (j == etiqueta) {
-							elementoCopia.remove();
-						}
-					}
-					localidad.conjuntoComplemento = copiaSecuencia.stream().mapToInt(Integer::valueOf).toArray();
-					break;
+		List<Integer> secuenciaBase;
+		try {
+			Scanner sc = new Scanner(new File(cargadorContexto.getResource(archivoQAPLIB).toURI()));
+			if (sc.hasNextInt()) {
+				cantidad = sc.nextInt();
+			}
+			for (int i = 0; i < cantidad; i++) {
+				distancias.add(new ArrayList<Integer>());
+				for (int j = 0; j < cantidad; j++) {
+					distancias.get(i).add(sc.nextInt());
 				}
 			}
-			localidad.etiqueta = String.valueOf(i);
-			localidad.distancias = localidadesEnCrudo[i - 1];
-			localidades.add(localidad);
+			for (int i = 0; i < cantidad; i++) {
+				flujos.add(new ArrayList<Integer>());
+				for (int j = 0; j < cantidad; j++) {
+					flujos.get(i).add(sc.nextInt());
+				}
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 
-		for (int i = 1; i <= localesEnCrudo.length; i++) {
+		secuenciaBase = IntStream.rangeClosed(1, cantidad).boxed().collect(Collectors.toList());
+
+		// 1) Eliminamos la diagonal cero.
+		for (int i = 0; i < flujos.size(); i++) {
+			for (int j = 0; j < flujos.get(i).size(); j++) {
+				if (i == j) { // Diagonal
+					int[] fila = flujos.get(i).stream().mapToInt(Integer::valueOf).toArray();
+					fila = ArrayUtils.remove(fila, j);
+					flujos.set(i, (ArrayList<Integer>) Arrays.stream(fila).boxed().collect(Collectors.toList()));
+				}
+			}
+		}
+		for (int i = 0; i < distancias.size(); i++) {
+			for (int j = 0; j < distancias.get(i).size(); j++) {
+				if (i == j) { // Diagonal
+					int[] fila = distancias.get(i).stream().mapToInt(Integer::valueOf).toArray();
+					fila = ArrayUtils.remove(fila, j);
+					distancias.set(i,(ArrayList<Integer>) Arrays.stream(fila).boxed().collect(Collectors.toList()));
+				}
+			}
+		}
+		// 2) Encapsulamos los objetos
+		for (int i = 1; i <= flujos.size(); i++) {
 			Local local = new Local();
-			int[] conjuntoComplemento = new int[secuencia.size() - 1];
+			int[] conjuntoComplemento = new int[cantidad - 1];
 			for (int j = 1; j <= conjuntoComplemento.length + 1; j++) {
-				if (i == secuencia.get(j - 1)) {
-					ArrayList<Integer> copiaSecuencia = new ArrayList<Integer>(secuencia);
+				if (i == secuenciaBase.get(j - 1)) {
+					ArrayList<Integer> copiaSecuencia = new ArrayList<Integer>(secuenciaBase);
 					for (Iterator<Integer> elementoCopia = copiaSecuencia.iterator(); elementoCopia.hasNext();) {
 						Integer etiqueta = elementoCopia.next();
 						if (j == etiqueta) {
@@ -102,9 +115,14 @@ public class BusquedaTabuNUG12 {
 				}
 			}
 			local.etiqueta = String.valueOf(i);
-			local.flujos = localesEnCrudo[i - 1];
+			local.flujos = flujos.get(i - 1).stream().mapToInt(Integer::valueOf).toArray();
 			locales.add(local);
-		}	
+		}
+		for (int i = 1; i <= distancias.size(); i++) {
+			Localidad localidad = new Localidad();
+			localidad.distancias = distancias.get(i - 1).stream().mapToInt(Integer::valueOf).toArray();
+			localidades.add(localidad);
+		}
 	}
 	
 	public static void grafico(String titulo, String linea1, String linea2, ArrayList<int[]> todos, ArrayList<int[]> mejores) {
