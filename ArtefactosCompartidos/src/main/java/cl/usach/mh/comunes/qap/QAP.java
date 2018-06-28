723a3f103a4773fbde9c254c069d1f68d83d8fd0
User <User@10.0.1.24>
2018-06-28 20:28:53 -0300

    ISaac: Añadida rutina de gráficos a la librería común.

--- ArtefactosCompartidos/src/main/java/cl/usach/mh/comunes/qap/QAP.java
+++ ArtefactosCompartidos/src/main/java/cl/usach/mh/comunes/qap/QAP.java
@@ -1,6 +1,23 @@
 package cl.usach.mh.comunes.qap;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import cl.usach.mh.comunes.qap.objetos.Local;
 import cl.usach.mh.comunes.qap.objetos.Localidad;	
@@ -9,6 +26,8 @@ public class QAP {
 	
 	private static ArrayList<Local> locales = new ArrayList<Local>();
 	private static ArrayList<Localidad> localidades = new ArrayList<Localidad>();
+	
+	private static int cantidad = 0;
 	
 	public QAP(ArrayList<Local> locales, ArrayList<Localidad> localidades) {
 		QAP.locales = locales;
@@ -36,7 +55,107 @@ public class QAP {
 		}	
         return costo; 
 	}
+	
+	public static void cargarDatosQAP(String archivoQAPLIB) throws URISyntaxException {
+		ClassLoader cargadorContexto = Thread.currentThread().getContextClassLoader();
 
+		ArrayList<ArrayList<Integer>> distancias = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> flujos = new ArrayList<ArrayList<Integer>>();
+		
+		List<Integer> secuenciaBase;
+		try {
+			Scanner sc = new Scanner(new File(cargadorContexto.getResource(archivoQAPLIB).toURI()));
+			if (sc.hasNextInt()) {
+				QAP.setCantidad(sc.nextInt());
+			}
+			for (int i = 0; i < QAP.getCantidad(); i++) {
+				distancias.add(new ArrayList<Integer>());
+				for (int j = 0; j < QAP.getCantidad(); j++) {
+					distancias.get(i).add(sc.nextInt());
+				}
+			}
+			for (int i = 0; i < QAP.getCantidad(); i++) {
+				flujos.add(new ArrayList<Integer>());
+				for (int j = 0; j < QAP.getCantidad(); j++) {
+					flujos.get(i).add(sc.nextInt());
+				}
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		secuenciaBase = IntStream.rangeClosed(1, QAP.getCantidad()).boxed().collect(Collectors.toList());
+
+		// 1) Eliminamos la diagonal cero.
+		for (int i = 0; i < flujos.size(); i++) {
+			for (int j = 0; j < flujos.get(i).size(); j++) {
+				if (i == j) { // Diagonal
+					int[] fila = flujos.get(i).stream().mapToInt(Integer::valueOf).toArray();
+					fila = ArrayUtils.remove(fila, j);
+					flujos.set(i, (ArrayList<Integer>) Arrays.stream(fila).boxed().collect(Collectors.toList()));
+				}
+			}
+		}
+		for (int i = 0; i < distancias.size(); i++) {
+			for (int j = 0; j < distancias.get(i).size(); j++) {
+				if (i == j) { // Diagonal
+					int[] fila = distancias.get(i).stream().mapToInt(Integer::valueOf).toArray();
+					fila = ArrayUtils.remove(fila, j);
+					distancias.set(i,(ArrayList<Integer>) Arrays.stream(fila).boxed().collect(Collectors.toList()));
+				}
+			}
+		}
+		// 2) Encapsulamos los objetos
+		for (int i = 1; i <= flujos.size(); i++) {
+			Local local = new Local();
+			int[] conjuntoComplemento = new int[QAP.getCantidad() - 1];
+			for (int j = 1; j <= conjuntoComplemento.length + 1; j++) {
+				if (i == secuenciaBase.get(j - 1)) {
+					ArrayList<Integer> copiaSecuencia = new ArrayList<Integer>(secuenciaBase);
+					for (Iterator<Integer> elementoCopia = copiaSecuencia.iterator(); elementoCopia.hasNext();) {
+						Integer etiqueta = elementoCopia.next();
+						if (j == etiqueta) {
+							elementoCopia.remove();
+						}
+					}
+					local.conjuntoComplemento = copiaSecuencia.stream().mapToInt(Integer::valueOf).toArray();
+					break;
+				}
+			}
+			local.etiqueta = String.valueOf(i);
+			local.flujos = flujos.get(i - 1).stream().mapToInt(Integer::valueOf).toArray();
+			QAP.getLocales().add(local);
+		}
+		for (int i = 1; i <= distancias.size(); i++) {
+			Localidad localidad = new Localidad();
+			localidad.distancias = distancias.get(i - 1).stream().mapToInt(Integer::valueOf).toArray();
+			QAP.getLocalidades().add(localidad);
+		}
+	}
+	
+	public static void grafico(String titulo, String linea1, String linea2, ArrayList<int[]> todos, ArrayList<int[]> mejores) {
+		final XYSeries serie1 = new XYSeries(linea1);
+		final XYSeries serie2 = new XYSeries(linea2);
+		for(int i=0;i<todos.size();i++) {
+			serie1.add(i, QAP.calculoCosto(todos.get(i)));
+		}
+		for(int i=0;i<mejores.size();i++) {
+			serie2.add(i, QAP.calculoCosto(mejores.get(i)));
+		}
+		final XYSeriesCollection datos = new XYSeriesCollection();
+		datos.addSeries(serie1);
+		datos.addSeries(serie2);
+		final JFreeChart chart = ChartFactory.createXYLineChart(titulo, "Iteraciones", "Costos", datos,
+				PlotOrientation.VERTICAL, true, true, false);
+
+		ChartFrame frame = new ChartFrame(titulo,chart);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	//
+	
 	public static ArrayList<Local> getLocales() {
 		return QAP.locales;
 	}
@@ -51,5 +170,13 @@ public class QAP {
 
 	public static void setLocalidades(ArrayList<Localidad> localidades) {
 		QAP.localidades = localidades;
+	}
+
+	public static int getCantidad() {
+		return cantidad;
+	}
+
+	public static void setCantidad(int cantidad) {
+		QAP.cantidad = cantidad;
 	}
 }
